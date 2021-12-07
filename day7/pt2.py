6299670181fbd53cc53b910f891348bb031d1891
Henry Price <hcprice115@gmail.com>
2021-12-07 12:54:49 -0500

    updated midpoint algorithim

--- day7/pt2.py
+++ day7/pt2.py
@@ -7,20 +7,17 @@ filename = os.path.join(here, 'input.txt')
 data = [int(x) for x in open(filename).read().split(',')]
 
 # Iterate through data, find mean num
-def find_mean(data):
-    mean = 0
-    for i in data:
-        mean += i
-    return math.floor(mean / len(data))
+def find_midpoint(data):
+    location = int((sum(data) + 1) / len(data))
+    return location
 
 def find_steps(data):
     fuel = 0
-    mean = find_mean(data)
+    mean = find_midpoint(data)
     for step in range(len(data)):
         diff = abs(data[step] - mean)
         for i in range(diff):
             fuel += i + 1
     return fuel
 
-print(find_mean(data))
 print(find_steps(data))
